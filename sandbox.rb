8f9a19492fa8ddaeaa1535e586bfc9367ccc48b8
Joe McIlvain <joe.eli.mac@gmail.com>
2013-08-29 01:51:47 -0400

    Based UDP firewall test in sandbox.rb

--- sandbox.rb
+++ sandbox.rb
@@ -2,6 +2,21 @@
 require_relative './udp.rb'
 
 tx = UDP::TX.new "224.0.1.33", 4567
+rx = UDP::RX.new "224.0.1.33", 4567
 
-tx.puts 'd'*(UDP.max_length+5)
+gthr = Thread.current
+passed = false
+thr = Thread.new do
+  p rx.gets
+  passed = true
+  gthr.wakeup
+end
+Thread.pass
+
+tx.puts 'test'
+
+sleep 1
+thr.kill
+
+raise IOError, "Probably firewalled..." unless passed
 
