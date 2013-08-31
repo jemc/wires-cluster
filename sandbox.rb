8eb4bf69f851ca563c6f36e42116309fe26b73e5
Joe McIlvain <joe.eli.mac@gmail.com>
2013-08-31 17:36:02 -0400

    Moved udp to lib/wires/cluster/
    --HG--
    rename : udp.rb => lib/wires/cluster/udp.rb

--- sandbox.rb
+++ sandbox.rb
@@ -1,5 +1,6 @@
-
-require_relative './udp.rb'
+$LOAD_PATH.unshift(File.expand_path("./lib", File.dirname(__FILE__)))
+require 'wires'
+require 'wires/cluster'
 
 tx = UDP::TX.new "224.0.1.33", 4567
 
--- sandbox2.rb
+++ sandbox2.rb
@@ -1,5 +1,6 @@
-
-require_relative './udp.rb'
+$LOAD_PATH.unshift(File.expand_path("./lib", File.dirname(__FILE__)))
+require 'wires'
+require 'wires/cluster'
 
 rx = UDP::RX.new "224.0.1.33", 4567
 raise IOError, "Probably firewalled..." unless rx.test!
