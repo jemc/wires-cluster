acde45369758d3edef4bca3ff6bdb44aa9738152
Joe McIlvain <joe.eli.mac@gmail.com>
2013-09-02 00:32:19 -0400

    Added JSON deserialization to the listen method

--- lib/wires/cluster.rb
+++ lib/wires/cluster.rb
@@ -15,24 +15,38 @@ module Wires
     def self.listen(action=:start)
       
       case action
-      
       when :start
+        
         @rx ||= Wires::Cluster::UDP::RX.new GROUP, PORT
         raise IOError, "Probably firewalled..." unless @rx.test!
         
         @rx_thread ||= Thread.new do
           loop do
             msg = @rx.gets
-            p [msg.size, msg, msg.source]
+            p [JSON.load(msg), msg.source]
           end
         end
       
       when :stop
         @rx_thread.kill; @rx_thread = nil
         @rx.close;       @rx        = nil
-      
       end
+      
     end
     
   end
+  
+  
+  # Reopen Event to specify serialization methodology
+  class Event
+    def to_json(*serialization_args)
+      { json_class:self.class.name, 
+        args:[*@args, **@kwargs] }.to_json(*serialization_args)
+    end
+    
+    def self.json_create(data)
+      self.new(*data['args'])
+    end
+  end
+  
 end
--- sandbox.rb
+++ sandbox.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.expand_path("./lib", File.dirname(__FILE__)))
+require 'active_support' # workaround for issue with activesupport-core_ext
 require 'wires/cluster'
 
 tx = Wires::Cluster::UDP::TX.new "224.0.1.33", 4567
 
-tx.puts 'something'
-tx.puts 'something_else'
+tx.puts JSON.dump(Wires::Event.new(55, 22, dog:true))
